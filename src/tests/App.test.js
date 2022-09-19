43cb5b8aeefba49793d92f5f378ed9d3928058c9
Marco Viana <marcoviana100@gmail.com>
2022-09-19 19:16:20 -0300

    Requisito 1.0

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import { Router } from 'react-router-dom';
 import App from '../App';
 import renderWithRouter from '../renderWithRouter';
 
