bdc958cabccba37346ebda4a007ad9e3e4cc8e62
Marco Viana <marcoviana100@gmail.com>
2022-09-19 23:24:46 -0300

    Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,30 @@
-test('', () => {});
+import React from 'react';
+import { act } from 'react-dom/test-utils';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Requisito 4', () => {
+  test('se a página contém um heading h2 com o texto Page requested not found', () => {
+    const { history } = renderWithRouter(<App />);
+    act(() => {
+      history.push('/rota-qualquer');
+    });
+    const textoNotFound = screen.getByRole(
+      'heading',
+      { name: 'Page requested not found', level: 2 },
+    );
+    expect(textoNotFound).toBeInTheDocument();
+  });
+
+  test('se a página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif', () => {
+    const { history } = renderWithRouter(<App />);
+    act(() => {
+      history.push('/rota-qualquer');
+    });
+    const image = screen.getByRole('img', {
+      name: /pikachu crying because the page requested was not found/i,
+    });
+    expect(image).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
