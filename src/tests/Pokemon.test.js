eda391bcabc9980e195a7410ad0eafeb3a071469
Marco Viana <marcoviana100@gmail.com>
2022-09-21 18:35:14 -0300

    Requisito 6

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import { act } from 'react-dom/test-utils';
 import App from '../App';
 import renderWithRouter from '../renderWithRouter';
 
@@ -9,14 +10,49 @@ describe('Requisito 6', () => {
     renderWithRouter(<App />);
     const buttonCharmander = screen.getByRole('button', { name: /fire/i });
     userEvent.click(buttonCharmander);
-    const nameCharmander = screen.getByText(/charmander/i);
-    const typeCharmander = screen.getByText(/charmander/i);
+
+    const nameCharmander = screen.getByTestId('pokemon-name');
+    const typeCharmander = screen.getByTestId('pokemon-type');
     const sizeCharmander = screen.getByText(/average weight: 8\.5 kg/i);
     const imageCharmander = screen.getByRole('img', {
       name: /charmander sprite/i });
+    const weightPokemon = screen.getByTestId('pokemon-weight');
+
+    expect(typeCharmander.innerHTML).toBe('Fire');
+    expect(weightPokemon).toBeInTheDocument();
     expect(nameCharmander).toBeInTheDocument();
     expect(typeCharmander).toBeInTheDocument();
     expect(sizeCharmander).toBeInTheDocument();
     expect(imageCharmander).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/0/0a/Spr_5b_004.png');
+  });
+
+  test('se o card do pok. indi. na Pokédex contém um link para exib. det. do pok', () => {
+    const { history } = renderWithRouter(<App />);
+    const linkURL = screen.getByRole('link', {
+      name: /more details/i,
+    });
+    expect(linkURL).toBeInTheDocument();
+    userEvent.click(linkURL);
+    const rota = history.location.pathname;
+    expect(rota).toBe('/pokemons/25');
+  });
+
+  test('Teste se existe um ícone de estrela nos pokémons favoritados', () => {
+    const { history } = renderWithRouter(<App />);
+    act(() => {
+      history.push('/pokemons/25');
+    });
+
+    const favorite = screen.getByRole('checkbox', {
+      name: /pokémon favoritado\?/i,
+    });
+    userEvent.click(favorite);
+
+    const favoriteIcon = screen.getByRole('img', {
+      name: /pikachu is marked as favorite/i,
+    });
+    expect(favoriteIcon).toBeInTheDocument();
+    expect(favoriteIcon).toHaveAttribute('src', '/star-icon.svg');
+    expect(favoriteIcon).toHaveAttribute('alt', 'Pikachu is marked as favorite');
   });
 });
